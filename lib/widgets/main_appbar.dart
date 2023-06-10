efd669e3308f85b41220dfd5f62ca8b327dafa82
Marcus Kalmar <marcus@videvit.se>
2023-06-10 10:55:00 +0200

    add some stuff

---
+++ lib/widgets/main_appbar.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MainAppBar extends StatelessWidget {
+  const MainAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      elevation: 2,
+      backgroundColor: Colors.teal[700],
+      child: Column(
+        children: [],
+      ),
+    );
+  }
+}
