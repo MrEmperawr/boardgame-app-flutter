5577e7c16bb53360cb4d7acebac055417bd744ab
Marcus Kalmar <marcus@videvit.se>
2023-06-10 11:13:11 +0200

    add localizations

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:boardgame_app_flutter/pages/home/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'package:boardgame_app_flutter/pages/home/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +13,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'), // English
+      ],
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.teal,
