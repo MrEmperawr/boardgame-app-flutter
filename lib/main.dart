813ad703bdcb3521cc52b05a5d2d467c7788b714
Marcus Kalmar <marcus@videvit.se>
2023-06-12 10:43:02 +0200

    add a repository provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:boardgame_app_flutter/clients/http_client.dart';
+import 'package:boardgame_app_flutter/services/games_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'package:boardgame_app_flutter/common/theme/app_theme.dart';
@@ -9,34 +12,43 @@ import 'package:boardgame_app_flutter/pages/home/home_page.dart';
 import 'generated/l10n.dart';
 
 void main() {
-  runApp(const MyApp());
+  final httpClient = HttpClient();
+  runApp(MyApp(
+    httpClient: httpClient,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final HttpClient httpClient;
+  const MyApp({super.key, required this.httpClient});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      localizationsDelegates: const [
-        S.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('en'),
-      ],
-      title: 'Flutter Demo',
-      theme: lightTheme,
-      darkTheme: darkTheme,
-      themeMode: ThemeMode.system,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const HomePage(),
-        '/games': (context) => const GamesPage(),
-        '/gameDetail': (context) => const GameDetailsPage(),
-      },
+    return RepositoryProvider(
+      create: (context) => GameService(httpClient),
+      child: MaterialApp(
+        localizationsDelegates: const [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale('en'),
+        ],
+        title: 'Flutter Demo',
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        themeMode: ThemeMode.system,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const HomePage(),
+          '/games': (context) => GamesPage(
+                service: context.read<GameService>(),
+              ),
+          '/gameDetail': (context) => const GameDetailsPage(),
+        },
+      ),
     );
   }
 }
